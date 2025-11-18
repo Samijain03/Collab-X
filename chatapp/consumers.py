31a5154791e909491025412a30e2be37962c5b16
Samay <Samijain2004@gmail.com>
2025-11-19 00:14:37 +0530

    fix workspace

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -440,3 +440,24 @@ class GroupChatConsumer(AsyncWebsocketConsumer):
         except GroupMessage.DoesNotExist:
             return None
         return None
+
+class WorkspaceConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.workspace_key = self.scope['url_route']['kwargs']['workspace_key']
+        self.room_group_name = f'workspace_{self.workspace_key}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        # Placeholder for workspace logic
+        pass
